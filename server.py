5e972dd674666cae635b02407902e6bf76f4b7c4
cre-w <145413026+cre-w@users.noreply.github.com>
2025-03-19 13:10:51 +0300

    flask

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
